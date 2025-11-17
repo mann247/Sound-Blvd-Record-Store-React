d4543aa63650c8e6c06a3cfb30cc69aac9c43a0a
N Manning <nmanning4@outlook.com>
2025-11-17 12:19:44 -0500

    Final edits

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ dotenv.config({ path: path.join(__dirname, '.env')});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const BUILD_PATH = path.join(__dirname, '../ecomm/dist');
+const BUILD_PATH = path.join(__dirname, '../ecomm/dist'); 
 
 app.use(cors());
 app.use(express.json());
