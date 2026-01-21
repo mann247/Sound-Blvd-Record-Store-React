11c38b9d6462df0d9e6f769e1b1acc13b4d3dd79
mann247 <nmanning4@outlook.com>
2026-01-20 22:04:32 -0500

    Updated Code

--- ecomm/src/components/Footer.jsx
+++ ecomm/src/components/Footer.jsx
@@ -8,9 +8,9 @@ class Footer extends React.Component{
         return (
            <footer className="social-media">
             <div className="social-media">
-            <a href="https://instagram.com/sound.blvd_" aria-label="Instagram" target="_blank"><img src={instagram} alt="instagram logo"/></a>
-            <a href="https://x.com/sound_blvd" aria-label="Twitter" target="_blank"><img src={twitter}alt="twitter logo"/></a>
-            <a href="https://medium.com/@ifsoundblvdcouldtalk" aria-label="Medium" target="_blank"><img src={medium}alt="medium logo"/></a>
+            <a href="https://www.instagram.com/" aria-label="Instagram" target="_blank"><img src={instagram} alt="instagram logo"/></a>
+            <a href="https://x.com/" aria-label="Twitter" target="_blank"><img src={twitter}alt="twitter logo"/></a>
+            <a href="https://medium.com/" aria-label="Medium" target="_blank"><img src={medium}alt="medium logo"/></a>
         </div>
           <small>© {new Date().getFullYear()} Sound Blvd. Records</small>
     </footer>
